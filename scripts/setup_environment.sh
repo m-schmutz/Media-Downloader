5a41b4a08fc9d93f2b0f737adb37c0534bd212a3
m-schmutz <marschm131@gmail.com>
2026-04-03 12:24:07 -0700

    uhh yea

--- scripts/setup_environment.sh
+++ scripts/setup_environment.sh
@@ -3,32 +3,53 @@
 # exit on command fail
 set -e
 
-# get the absolute path to where the script is located
+# absolute path of directory containing this script 
 declare -r SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# get the absolute path of project directory
+# absolute path of project directory
 declare -r PROJECT_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
 
+# absolute path of 'binaries' directory
+declare -r BINARIES_DIR="$PROJECT_DIR/binaries"
 
 
 
 
-# server environment directory
-declare -r DLP_ENV="$PROJECT_DIR/dlp-venv"
 
-# server environment pip
-declare -r DLP_ENV_PIP="$DLP_ENV/bin/pip3"
-
-# server environment requirements
-declare -r DLP_REQUIREMENTS="$PROJECT_DIR/requirements/dlp.txt"
-
-
-
-# ensure that the python environment exists
-if [[ ! -d "$DLP_ENV" ]]; then
-    python3 -m venv "$DLP_ENV"
-    "$DLP_ENV_PIP" install -U yt-dlp-ejs
+# if yt-dlp binary is not downloaded, download it and set to be executable
+if [[ ! -f "$BINARIES_DIR/yt-dlp" ]]; then
+    curl -L "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp" -o "$BINARIES_DIR/yt-dlp"
+    chmod +x "$BINARIES_DIR/yt-dlp"
 fi
+
+
+# if deno binary is not downloaded, download it 
+if [[ ! -f "$BINARIES_DIR/yt-dlp" ]]; then
+    curl -L "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp" -o "$BINARIES_DIR/yt-dlp"
+    chmod +x "$BINARIES_DIR/yt-dlp"
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
